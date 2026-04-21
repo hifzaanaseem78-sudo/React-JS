076f06f7cf1075128fd761b38f86c1f3e6fa3cc6
Hifza Naseem <hifzaanaseem78@gmail.com>
2026-04-21 22:08:20 +0500

    created custom react

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,12 @@
 import Chai from "./Hifza"
 
 function App() {
+const username = "chai aur code";
   
 
   return (
     <>
-    <h1>React with vite | Hifza Naseem</h1>
+    <h1>React with vite | Hifza Naseem {username}</h1>
     <Chai/>
     </>
   )
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,9 +1,47 @@
+import React from 'react'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom App!</h1>
+        </div>
+    )
+}
+
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: "_blank"
+    },
+    children: "click me to visit google"
+}
+
+const anotherElement = (
+    <a href="https://google.com" target="_blank">Visit Google</a>
+)
+
+const anotherUser = "chai aur react";
+const User = "2";
+
+const reactElement = React.createElement(
+    "a",
+    {
+        href: "https://google.com", 
+        target: "_blank"
+    },
+    "click me to visit google",
+    anotherUser,
+    User
+ )
+
+
 createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
+  //<App />
+  reactElement
+
 )
